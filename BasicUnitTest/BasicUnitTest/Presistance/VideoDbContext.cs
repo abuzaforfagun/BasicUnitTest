b0b9fc914e4f29f200d867567f309aea8c20c26e
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2018-02-05 12:13:28 +0600

    Add Entity Framework

---
+++ BasicUnitTest/BasicUnitTest/Presistance/VideoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BasicUnitTest.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicUnitTest.Presistance
+{
+    class VideoDbContext:DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public VideoDbContext()
+        {
+
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=VideoDb;Trusted_Connection=True;");
+        }
+    }
+}
