28593c0177baf9b885d82d2e85294ecafe11d793
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2018-02-08 19:47:09 +0600

    Fix bug of detact overlapping

--- BasicUnitTest/BasicUnitTest/Services/BookingService.cs
+++ BasicUnitTest/BasicUnitTest/Services/BookingService.cs
@@ -17,10 +17,8 @@ namespace BasicUnitTest.Services
             var overlappingBooking =
                 bookings.FirstOrDefault(
                     b =>
-                        booking.ArrivalDate >= b.ArrivalDate
-                        && booking.ArrivalDate < b.DepartureDate
-                        || booking.DepartureDate > b.ArrivalDate
-                        && booking.DepartureDate <= b.DepartureDate);
+                        booking.ArrivalDate < b.DepartureDate
+                        && b.ArrivalDate < booking.DepartureDate);
 
             return overlappingBooking == null ? string.Empty : overlappingBooking.Reference;
         }
